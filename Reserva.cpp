cf7a22868c2a57e39ae5d0189a7ba8333eba0002
santiago fernandez fernandez <santifernandez0902@gmail.com>
2022-09-18 22:04:36 -0500

    New: adding new methods to Reserva

--- Reserva.cpp
+++ Reserva.cpp
@@ -3,3 +3,29 @@
 //
 
 #include "Reserva.h"
+
+Reserva::Reserva() {
+    cout << "Entre al constructor por defecto \n";
+    fechaInicial = "";
+    fechaFinal = "";
+}
+
+const string &Reserva::getfechaInicial() const {
+    return fechaInicial;
+}
+
+void Reserva::setfechaInicial(const std::string &fechaInicial) {
+    Reserva::fechaInicial = fechaInicial;
+}
+
+const string &Reserva::getfechaFinal() const {
+    return fechaFinal;
+}
+
+void Reserva::setfechaFinal(const std::string &fechaFinal) {
+    Reserva::fechaFinal = fechaFinal;
+}
+
+Reserva::~Reserva() {
+
+}
--- Reserva.h
+++ Reserva.h
@@ -4,9 +4,25 @@
 
 #ifndef AIRBNB_RESERVA_H
 #define AIRBNB_RESERVA_H
-
+#include <iostream>
+#include <string>
+using namespace std;
 
 class Reserva {
+private:
+    string fechaInicial;
+    string fechaFinal;
+    //Propietario *propietario;
+    //Huesped *huesped;
+public:
+    Reserva();
+    const string &getfechaInicial() const;
+    const string &getfechaFinal() const;
+    void setfechaInicial(const string &fechaInicial);
+    void setfechaFinal(const string &fechaFinal);
+
+    virtual ~Reserva();
+
 
 };
 
