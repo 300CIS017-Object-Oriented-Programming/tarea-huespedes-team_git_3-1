6c96a7d300035c43e732e20c131b479dabd838c5
santiago fernandez fernandez <santifernandez0902@gmail.com>
2022-09-18 22:28:29 -0500

    New: adding new methods to Persona

--- Persona.cpp
+++ Persona.cpp
@@ -3,3 +3,47 @@
 //
 
 #include "Persona.h"
+
+Persona::Persona() {
+    cout << "Entre al constructor por defecto \n";
+    idPersona = 0;
+    nombre = "";
+    sexo = "";
+    puntaje = 0;
+}
+
+long Persona::getidPersona() const {
+    return idPersona;
+}
+
+const string &Persona::getnombre() const {
+    return nombre;
+}
+
+const string &Persona::getsexo() const {
+    return sexo;
+}
+
+int Persona::getpuntaje() const {
+    return puntaje;
+}
+
+void Persona::setidPersona(long idPersona) {
+    Persona::idPersona = idPersona;
+}
+
+void Persona::setnombre(const std::string &nombre) {
+    Persona::nombre = nombre;
+}
+
+void Persona::setsexo(const std::string &sexo) {
+    Persona::sexo = sexo;
+}
+
+void Persona::setpuntaje(int puntaje) {
+    Persona::puntaje = puntaje;
+}
+
+Persona::~Persona() {
+
+}
--- Persona.h
+++ Persona.h
@@ -4,9 +4,31 @@
 
 #ifndef AIRBNB_PERSONA_H
 #define AIRBNB_PERSONA_H
+#include <iostream>
+#include <string>
+using namespace std;
 
 
 class Persona {
+private:
+    long idPersona;
+    string nombre;
+    string sexo;
+    int puntaje;
+public:
+    Persona();
+
+    long getidPersona() const;
+    const string &getnombre() const;
+    const string &getsexo() const;
+    int getpuntaje() const;
+
+    void setidPersona(long idPersona);
+    void setnombre(const string &nombre);
+    void setsexo(const string &sexo);
+    void setpuntaje(int puntaje);
+
+    virtual ~Persona();
 
 };
 
