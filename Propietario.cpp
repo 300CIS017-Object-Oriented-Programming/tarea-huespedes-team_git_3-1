e5139c908f099dd3884f03f63929979ad61f8712
Jose <danielramirezdelgado32@gmail.com>
2022-09-19 21:54:46 -0500

    New: se crean nuevops emtodos en la clase Propietario

--- Propietario.cpp
+++ Propietario.cpp
@@ -10,4 +10,28 @@ Propietario::Propietario() : Persona()
     disponible = false;
 }
 
-Propietario::Propietario(Hogar *hogar, bool disponible) : Persona(idPersona, nombre, sexo, puntaje){}
+Propietario::Propietario(Hogar *hogar, bool disponible) : Persona(idPersona, nombre, sexo, puntaje, fechaNacimiento)
+{
+    this->hogar = hogar;
+    this->disponible = disponible;
+}
+
+void Propietario::setHogar(Hogar *hogar1)
+{
+    Propietario::hogar=hogar1;
+}
+
+void Propietario::setDisponible(bool disponible1)
+{
+    Propietario::disponible = disponible1;
+}
+
+Hogar* Propietario::getHogar() const
+{
+    return hogar;
+}
+
+bool Propietario::getDisponible() const
+{
+    return disponible;
+}
